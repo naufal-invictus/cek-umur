0e1b505fd17f009609f56f784fab89d75540baf2
naufal-invictus <143852075+naufal-invictus@users.noreply.github.com>
2024-06-24 00:28:43 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+function hitungUmur() {
+    var umur = document.getElementById('umurInput').value;
+    
+    
+    
+
+    document.getElementById('hasilUmur').innerText = "Umur Anda adalah: " + umur + " tahun.";
+}
